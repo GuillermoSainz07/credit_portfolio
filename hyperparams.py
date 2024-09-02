445839beaa4657a099f2dbf306927d168b59cb23
GuillermoSainz07 <86427154+GuillermoSainz07@users.noreply.github.com>
2024-09-02 12:28:59 -0700

    add changes

---
+++ hyperparams.py
@@ -0,0 +1,5 @@
+params = {'booster': 'gbtree',
+ 'lambda': 0.028052595131969085,
+ 'alpha': 8.989738506909321e-07,
+ 'subsample': 0.9547205774093543,
+ 'colsample_bytree': 0.3622106755002568}
--- run_experiments.py
+++ run_experiments.py
@@ -1,9 +1,67 @@
-import os
+import pandas as pd
+import numpy as np
+
 import mlflow
+import xgboost as xgb
+from hyperparams import params
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import TargetEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import log_loss, accuracy_score, f1_score
+
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
+data = pd.read_csv('credit_risk_dataset.csv')
+data = data.drop(index=data.query('person_emp_length > 100').index)
+
+X_cat = data.drop(columns=['loan_status','cb_person_default_on_file',
+                           'person_home_ownership']).select_dtypes(include=['object'])
+
+X_num = data.drop(columns='loan_status').select_dtypes(include=['float','int'])
+y = data.loan_status.to_numpy().flatten()
+
+X = X_num.join(X_cat)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, stratify=y)
+
+X_train,X_val, y_train, y_val = train_test_split(X_train,y_train,stratify=y_train, train_size=0.70)
+
+col_trans = ColumnTransformer([("cat_preprocess", TargetEncoder(), ["loan_grade"]),
+                                ("cat_preprocess_2", TargetEncoder(), ["loan_intent"])],
+                                remainder='passthrough').set_output(transform='pandas')
+
+col_trans.fit_transform(X_train,y_train)
+
+
 TRACKING_SERVER_HOST = "ec2-3-133-151-150.us-east-2.compute.amazonaws.com"
 mlflow.set_tracking_uri(f'http://{TRACKING_SERVER_HOST}:5000')
+print(f'Tracking Server URI: {mlflow.get_tracking_uri()}')
+mlflow.set_experiment('xgb_pipeline_credit_model')
 
+
+with mlflow.start_run(run_name='XGB Credit Classifier'):
+    xgb_clf = xgb.XGBClassifier(**params,
+                               early_stopping_rounds=20).set_fit_request(eval_set=True)
+
+    xgb_clf.fit(X_train,y_train,
+                eval_set=[(X_val, y_val)],
+                verbose=5)
+    
+    pipe = Pipeline([('transformer',col_trans),
+                 ('xgb_model',xgb_clf)])
+    
+    y_pred = xgb_clf.predict(X_test)
+    y_pred_probabilities = xgb_clf.predict_proba(X_test)[:, 1]
+
+    mlflow.log_params(xgb_clf.get_params())
+    mlflow.log_metric('log_loss', accuracy_score(y, y_pred_probabilities))
+    mlflow.log_metric('accuracy', accuracy_score(y, y_pred))
+    mlflow.log_metric('f1_score', f1_score(y, y_pred))
+    mlflow.xgboost.log_model(xgb_clf,'xgb_model')
+    mlflow.sklearn.log_model(pipe, "xgb_pipeline")
+    
